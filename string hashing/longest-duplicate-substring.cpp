6f6755c3196de5d3b6e964ee5330dd0b42b26ad9
TANISH MOHANTA <tanishmohanta1901@gmail.com>
2023-04-20 12:23:51 +0530

    longest duplicate substring

---
+++ string hashing/longest-duplicate-substring.cpp
@@ -0,0 +1,126 @@
+#define ll long long int
+#define mod 1000000007
+
+struct hash_pair {
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2>& p) const
+    {
+        auto hash1 = hash<T1>{}(p.first);
+        auto hash2 = hash<T2>{}(p.second);
+ 
+        if (hash1 != hash2) {
+            return hash1 ^ hash2;             
+        }
+         
+        // If hash1 == hash2, their XOR is zero.
+          return hash1;
+    }
+};
+
+ll power(ll a , ll b)
+{
+    ll ans = 1;
+    while(b)
+    {
+        if(b&1) ans = (ans%mod * a%mod)%mod;
+        a=(a%mod * a%mod)%mod;
+        b=b>>1;
+    }
+    return ans;
+}
+    string longestDupSubstring(string s) {
+        int n=s.size();
+
+        vector<pair<ll,ll>> hash(n);
+
+        ll p1=31 , p2=37 , m1=1 , m2=1;
+        vector<ll> pow_p1(n) , pow_p2(n);
+
+        for(int i=0 ; i<n ; i++)
+        {
+            pow_p1[i] = m1;
+            pow_p2[i] = m2;
+            ll hash1 = ((s[i] - 'a' + 1)*m1 )%mod;
+            ll hash2 = ((s[i] - 'a' + 1)*m2 )%mod;
+            m1 = (m1*p1)%mod;
+            m2 = (m2*p2)%mod;
+
+            if(i)
+            {
+                hash1 = (hash1 + hash[i-1].first) % mod;
+                hash2 = (hash2 + hash[i-1].second) % mod;
+            }
+            hash[i] = {hash1 , hash2};
+        }
+
+        vector<ll> inv_p1(n) , inv_p2(n);
+
+        // for(int i=0 ; i<n ; i++)
+        // {
+        //     inv_p1[i] = power(pow_p1[i] , mod-2);
+        //     inv_p2[i] = power(pow_p2[i] , mod-2);
+        // }
+
+        int pw_inv1=power(p1, mod-2 );
+        int pw_inv2=power(p2, mod-2 );
+        inv_p1[0]=1;
+        inv_p2[0]=1;
+        for(int i=1 ; i<n; i++)
+        {
+            inv_p1[i]=(inv_p1[i-1] * pw_inv1)% mod;
+            inv_p2[i]=(inv_p2[i-1] * pw_inv2)% mod;
+        }
+
+        auto getHash = [&](ll l ,ll r){
+                ll hash1 = hash[r].first , hash2 = hash[r].second;
+                if(l){
+                    hash1 = (hash1 - hash[l-1].first + mod)%mod;
+                    hash2 = (hash2 - hash[l-1].second + mod)%mod;
+                    hash1 = (hash1 * inv_p1[l])%mod;
+                    hash2 = (hash2 * inv_p2[l])%mod;
+                }
+                return make_pair(hash1 , hash2);
+            };
+            
+        
+        
+        int low= 0;
+        int high= n-2, ii=-1, jj=-1;
+        string f= "";
+        
+        
+        while(low<=high)
+        {
+            unordered_map<pair<ll, ll>, ll, hash_pair> mp;
+            int mid= low+ (high-low)/2;
+            int got= 0;
+            for(int i=0; i<n-mid; i++)
+            {
+                auto h=getHash(i,i+mid);
+                mp[h]++;
+                if(mp[h]>1)
+                {
+                    got= 1;
+                    ii= i;
+                    jj= i + mid; 
+                    //return f;
+                }
+            }
+            if(got)
+            {
+                
+                low= mid+ 1;
+            }
+            else
+            {
+                high= mid- 1;
+            }
+        }
+        
+        f="";
+        for(int k=ii; k<=jj && ii!=-1 ;k++)
+            {
+                f.push_back(s[k] );
+            }
+        return f
+    }
